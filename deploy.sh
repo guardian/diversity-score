c1e7a0accc7819d5e6bcfa8d3dc837ce2a105558
Amy Hughes <amy.hughes@guardian.co.uk>
2020-11-26 11:27:35 +0000

    temp fix for bad URI construction, a little error logging

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 pushd "$CURRENT_DIR" > /dev/null || exit 1
 
-docker build -t amy-diversity-score -t 702972749545.dkr.ecr.eu-west-1.amazonaws.com/amy-diversity-score:latest .
-docker tag amy-diversity-score:latest 702972749545.dkr.ecr.eu-west-1.amazonaws.com/amy-diversity-score:latest
+commitId=$(git rev-parse --short HEAD)
+sbt assembly
+docker build -t "702972749545.dkr.ecr.eu-west-1.amazonaws.com/amy-diversity-score:latest" -t "amy-diversity-score:$commitId" -t "702972749545.dkr.ecr.eu-west-1.amazonaws.com/amy-diversity-score:$commitId" .
+echo $(aws ecr get-login-password --region eu-west-1 --profile developerPlayground) | docker login --password-stdin -u AWS https://702972749545.dkr.ecr.eu-west-1.amazonaws.com
+docker push "702972749545.dkr.ecr.eu-west-1.amazonaws.com/amy-diversity-score"
+popd
--- src/main/scala/lib/GenderService.scala
+++ src/main/scala/lib/GenderService.scala
@@ -5,6 +5,7 @@ import io.circe.parser.decode
 import sttp.client.{HttpURLConnectionBackend, basicRequest}
 import sttp.client._
 import io.circe.generic.auto._
+import sttp.model.Uri
 
 sealed trait Gender {
   def toString: String
@@ -32,9 +33,18 @@ object GenderService extends GenderService {
   implicit lazy val backend = HttpURLConnectionBackend()
 
   def genders(name: Name): IO[GenderResult] = {
-    val request = basicRequest.get(uri"https://api.genderize.io/?name=${name.name}")
+    //TODO: fix bug where when text ends with name, get %00%00%00%00%00%0... added to end of uri
+    val nm = name.name
+    val uri: Uri = uri"https://api.genderize.io/".param("name", nm.toString)
+//    println(s"uri: $uri")
 
-    def getResult(): IO[String] = IO.fromEither(request.send().body.left.map(_ => GenderServiceError))
+    val request = basicRequest.get(uri)
+//    println(s"request uri: ${request.uri}")
+
+    def getResult(): IO[String] = IO.fromEither(request.send().body.left.map{e =>
+      println(s"err: $e")
+      GenderServiceError
+    })
     def decodeGender(json: String): IO[GenderizeResult] = IO.fromEither(decode[GenderizeResult](json))
     // what is the difference between IO.pure and IO.apply??
     def getGender(genderRes: GenderizeResult): IO[GenderResult] = IO{
